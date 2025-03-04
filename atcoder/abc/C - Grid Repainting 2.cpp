6995d196bf9d2e237299807e958140bc383a369a
Kelvin <kelvinyu1117@gmail.com>
2025-03-04 21:44:17 +1100

    solved 2 problems from Atcoder

---
+++ atcoder/abc/C - Grid Repainting 2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int h, w;
+    cin >> h >> w;
+    
+    std::vector<std::vector<char>> grid(h + 1, std::vector<char>(w + 1, '.'));
+    int si, sj;
+    for(int i = 1; i <= h; i++) {
+        for(int j = 1; j <= w; j++) {
+            std::cin >> grid[i][j];
+        }
+    }
+    
+    
+    for(int i = 1; i < h; i++) {
+        for(int j = 1; j < w; j++) {
+            if (grid[i][j] == '#' && grid[i][j - 1] != '#' && grid[i - 1][j] != '#' && grid[i][j + 1] != '#' && grid[i + 1][j] != '#') {
+                cout << "No";
+                return 0;
+            }
+        }
+    }
+    
+    cout << "Yes";
+    return 0;
+}
---
+++ atcoder/abc/C - Rotation.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+
+int main()
+{
+    int n, q;
+    cin >> n >> q;
+    std::vector<char> dq;
+    for(int i = 0; i < n; i++) {
+        char c;
+        cin >> c;
+        dq.push_back(c);
+    }
+    
+    int shift = 0;
+    for(int i = 0; i < q; i++) {
+        int a, x;
+        cin >> a >> x;
+        if(a == 1) {
+            shift = (shift + n - x) % n;
+        } else {
+            cout << dq[(shift + x - 1) % n] << '\n';
+        }
+    }
+    
+    return 0;
+}
